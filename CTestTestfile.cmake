baa282fe780d19bfc51124c0aa61b3ddda5cf8d0
netw3173 <113054391+netw3173@users.noreply.github.com>
2022-11-08 10:54:51 -0500

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/unbunt20/Desktop/pitcoinnewdatecd/src
+# Build directory: /home/unbunt20/Desktop/pitcoinnewdatecd/build/release/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
